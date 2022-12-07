87d30239881b33f1c79766bf0b76186be412a775
midCenturyDude7 <mgriffes@gmail.com>
2022-12-07 15:04:01 -0800

    Removed section that was commented out

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     document.getElementById("bored__idea-container").innerHTML = `
-//       <h1 class="bored__idea-title">${data.activity}</h1>
-//     `;
-//   });
-
 document.getElementById("bored__idea-btn").addEventListener("click", () => {
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then((response) => response.json())
