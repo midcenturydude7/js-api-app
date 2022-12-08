265c4cd607bee89cfe2ecd09e52ea801532e6186
midCenturyDude7 <mgriffes@gmail.com>
2022-12-07 20:50:16 -0800

    Updated styles to fire on user behavior | click

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ document.getElementById("bored__idea-btn").addEventListener("click", () => {
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       document.getElementById("bored__idea-text").innerHTML = data.activity;
+      const grabElement = document.querySelector(".bored__bot-header");
+      grabElement.classList.add("bored__bot-header--fun");
     });
 });
