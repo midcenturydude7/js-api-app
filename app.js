9ed41ad4141b5f47d05e2c081e2f242f3ca77244
midCenturyDude7 <mgriffes@gmail.com>
2022-12-07 21:30:32 -0800

    Added text based on btn click

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ document.getElementById("bored__idea-btn").addEventListener("click", () => {
       document.getElementById("bored__idea-text").innerHTML = data.activity;
       const grabElement = document.querySelector(".bored__bot-header");
       grabElement.classList.add("bored__bot-header--fun");
+      addText();
     });
 });
+
+const addText = () => {
+  document.querySelector(".bored__bot-header").innerHTML = `
+    <div class="bored__bot-header--repeat">
+      <h1 class="bored__bot-title">BoredBot</h1><br>
+      <p class="bored__bot-header-text">Get ready for some inspiring things to do!</p>
+    </div>
+  `;
+};
