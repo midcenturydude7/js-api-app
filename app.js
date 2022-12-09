87f4763f367c5df77eaa7dc7640361d07d306039
midCenturyDude7 <mgriffes@gmail.com>
2022-12-08 20:57:42 -0800

    Fetched blog title and description and added to page

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-document.getElementById("bored__idea-btn").addEventListener("click", () => {
-  fetch("https://apis.scrimba.com/bored/api/activity")
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById("bored__idea-text").innerHTML = data.activity;
-      const grabElement = document.querySelector(".bored__bot-header");
-      grabElement.classList.add("bored__bot-header--fun");
-      addText();
-    });
-});
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    const postsArr = data.slice(0, 5);
 
-const addText = () => {
-  document.querySelector(".bored__bot-header").innerHTML = `
-    <div class="bored__bot-header--repeat">
-      <h1 class="bored__bot-title">BoredBot</h1><br>
-      <p class="bored__bot-header-text">Get ready for some inspiring things to do!</p>
-    </div>
-  `;
-};
+    for (let post of postsArr) {
+      document.getElementById("main__blog-post").innerHTML += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+      `;
+    }
+  });
