054aedc4f6eb855a31bc08d6a10180ab40243b8a
midCenturyDude7 <mgriffes@gmail.com>
2022-12-11 11:51:49 -0800

    GET request call to api | logged result to console

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-url = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/";
+const url = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/";
 
 document.getElementById("deck-btn").addEventListener("click", (e) => {
   e.preventDefault;
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 });
-
-
